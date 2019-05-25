62f553bab936abaf6acacc13c7926e2e49275a27
ClemClem <clement.berthet013@gmail.com>
2019-05-25 16:10:26 +0200

    test script

---
+++ MonScript.r
@@ -0,0 +1,30 @@
+###REG SIMPLE 
+
+#Ex : rendement mais 
+
+#Y : rendement 
+Y = c(16,
+      18,
+      23,
+      24,
+      28,
+      29,
+      26,
+      31,
+      32,
+      34) ; Y[1:3] 
+
+#X : quantit? engrais 
+
+X = c(20,
+      24,
+      28,
+      22,
+      32,
+      28,
+      32,
+      36,
+      41,
+      41) ; X[1:3] 
+
+library(ggplot2)
